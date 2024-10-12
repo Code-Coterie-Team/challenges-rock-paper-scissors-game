af80590f481b5199231099cbc34d74e262ffebec
faeze <faezeh.am4212@gmail.com>
2024-10-12 13:08:49 +0330

    AddAnimationToModal

--- game.js
+++ game.js
@@ -7,9 +7,14 @@ let close = document.getElementsByClassName("close")[0];
 
 rules.addEventListener("click", () => {
     modal.style.display = "block";
+    modal.style.animationName ="moveModalOpen";
+    modal.style.animationDuration ="1s";
 });
 
 close.addEventListener("click", () => {
+   
+    modal.style.animationName ="moveModalClose";
+    modal.style.animationDuration ="1s";
     modal.style.display = "none";
 });
 
